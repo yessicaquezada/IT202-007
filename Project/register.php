8d18ed76770a093536c8402557056ebf2e5c93e5
Yessica Quezada <ysq2@njit.edu>
2022-11-28 14:06:23 -0500

    User Registration

---
+++ Project/register.php
@@ -0,0 +1,94 @@
+<?php
+require_once(__DIR__ . "/../lib/functions.php");
+//TODO: 0.1: require_once(__DIR__ . "/../partials/nav.php");
+?>
+<form onsubmit="return validate(this)" method="POST">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" required />
+    </div>
+    <div>
+        <label for="pw">Password</label>
+        <input type="password" id="pw" name="password" required minlength="8" />
+    </div>
+    <div>
+        <label for="confirm">Confirm</label>
+        <input type="password" name="confirm" required minlength="8" />
+    </div>
+    <input type="submit" value="Register" />
+</form>
+<script>
+    function validate(form) {
+        //TODO 1: implement JavaScript validation
+        //ensure it returns false for an error and true for success
+
+        return true;
+    }
+</script>
+<?php
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
+    //$email = $_POST["email"];
+    //$password = $_POST["password"];
+    //$confirm = $_POST["confirm"];
+    
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
+    $confirm = se($_POST, "confirm", "", false);
+
+    //TODO 3.0
+    $hasError = false;
+    if (empty($email)) {
+        echo "Email must not be empty";
+        $hasError = true;
+    }
+    //sanitize
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    //TODO 4.1 replace 4.0: $email = sanitize_email($email);
+    //validate
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //TODO 4.1: if (!is_valid_email($email)) {
+    //TODO 4.2:     flash("Username must only contain 3-16 characters a-z, 0-9, _, or -", "danger");
+        echo "Invalid email address";
+        $hasError = true;
+            }
+
+    if (empty($password)) {
+        echo "Password must not be empty";
+        $hasError = true;
+    }
+    if (empty($confirm)) {
+        echo "Confirm password must not be empty";
+        $hasError = true;
+    }
+    if (strlen($password) < 8) {
+        echo "Password must be >8 characters";
+        $hasError = true;
+    }
+    if (strlen($password) > 0 && $password !== $confirm) {
+        echo "Passwords must match";
+        $hasError = true;
+    }
+    if (!$hasError) {
+        echo "Welcome, $email";
+        //TODO 5.0 $hash = password_hash($password, PASSWORD_BCRYPT);
+        //TODO 5.0 $db = getDB();
+        //TODO 5.0 $stmt = $db->prepare("INSERT INTO User (email, pwrdHash) VALUES(:email, :password)");
+        //TODO 5.0 try {
+        //TODO 5.0     $stmt->execute([":email" => $email, ":password" => $hash]);
+        //TODO 5.0     echo "Successfully registered!";
+        //TODO 5.1     echo with: flash("Successfully registered!", "success");
+        //TODO 5.0 } catch (Exception $e) {
+        //TODO 5.0    echo "There was a problem registering<br>";
+        //TODO 5.0    echo "<pre>" . var_export($e, true) . "</pre>";
+        //TODO 5.1    users_check_duplicate($e->errorInfo);
+        //TODO 5.0 } 
+    }
+}
+?>
+<!-- TODO 5.1: adding flash() -->
+<?php
+//require(__DIR__ . "/../../partials/flash.php");
+?>
