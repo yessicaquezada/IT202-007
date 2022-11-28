e08d5a93647a920c73c647f96f6ecd1c051b861b
Yessica Quezada <ysq2@njit.edu>
2022-11-28 17:03:26 -0500

    Basic Nav

--- Project/login.php
+++ Project/login.php
@@ -66,6 +66,10 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     unset($user["pwrdHash"]);
                     if (password_verify($password, $hash)) {
                         echo "Welcome $email";
+
+                    $_SESSION["user"] = $user;
+                    die(header("Location: home.php"));
+
                     } else {
                         echo "Invalid password";
                     }
--- partials/nav.php
+++ partials/nav.php
@@ -2,13 +2,14 @@
 session_start();
 //session_start(); must be the very first line after the <?php otherwise $_SESSION does not persist 
 //Note: this is to resolve cookie issues with port numbers
-/*TODO 1
+
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
 $localWorks = true; //some people have issues with localhost for the cookie params
 //if you're one of those people make this false
+
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
 if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
@@ -24,7 +25,7 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
 //include functions here so we can have it on every page that uses the nav bar
 //that way we don't need to include so many other files on each page
 //nav will pull in functions and functions will pull in db
-*/
+
 require(__DIR__ . "/../lib/functions.php");
 ?>
 <nav>
