f1c9042a14e5cd8c25a80f5b8addb2a1aa872d8a
Yessica Quezada <ysq2@njit.edu>
2022-11-29 00:10:05 -0500

    Update

--- lib/flash_messages.php
+++ lib/flash_messages.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function flash($msg = "", $color = "info")
 {
@@ -20,26 +19,3 @@ function getMessages()
     }
     return array();
 }
-=======
-<?php
-function flash($msg = "", $color = "info")
-{
-    $message = ["text" => $msg, "color" => $color];
-    if (isset($_SESSION['flash'])) {
-        array_push($_SESSION['flash'], $message);
-    } else {
-        $_SESSION['flash'] = array();
-        array_push($_SESSION['flash'], $message);
-    }
-}
-
-function getMessages()
-{
-    if (isset($_SESSION['flash'])) {
-        $flashes = $_SESSION['flash'];
-        $_SESSION['flash'] = array();
-        return $flashes;
-    }
-    return array();
-}
->>>>>>> Feat-FlashMessages
