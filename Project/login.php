57e5f45566e24cd9a4f5fae79af95e41d7f6ed37
Yessica Quezada <ysq2@njit.edu>
2022-11-29 01:26:25 -0500

    Milestone1

--- Project/login.php
+++ Project/login.php
@@ -26,38 +26,37 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
 
-    //TODO 3.0
+    //TODO 3
     $hasError = false;
     if (empty($email)) {
-        //TODO 3.1 flash("Email must not be empty", "danger");
         flash("Email must not be empty");
         $hasError = true;
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //TODO 4.1: if (!is_valid_email($email)) {
-    //TODO 4.2:     flash("Username must only contain 3-16 characters a-z, 0-9, _, or -", "danger");
-         flash("Invalid email address");
-         $hasError = true;
-     }
-
-    if (empty($password)) {
-        flash("Password must not be empty");
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        flash("Invalid email address");
+        $hasError = true;
+    }*/
+    if (!is_valid_email($email)) {
+        flash("Invalid email address");
         $hasError = true;
     }
-    if (strlen($password) < 8) {
-        flash("Password must be >8 characters");
+    if (empty($password)) {
+        flash("password must not be empty");
+        $hasError = true;
+    }
+    if (!is_valid_password($password)) {
+        flash("Password too short");
         $hasError = true;
     }
     if (!$hasError) {
-        flash("Welcome, $email");
-        //TODO 4.0
-
+        //flash("Welcome, $email");
+        //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, logName from User where id = :id LIMIT 1");
+        $stmt = $db->prepare("SELECT id, email, logName, pwrdHash from User where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -66,10 +65,10 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["pwrdHash"];
                     unset($user["pwrdHash"]);
                     if (password_verify($password, $hash)) {
-                        flash("Welcome $email");
-
-                $_SESSION["user"] = $user;
-                die(header("Location: home.php"));
+                        //flash("Weclome $email");
+                        $_SESSION["user"] = $user; //sets our session data from db
+                        flash("Welcome, " . get_username());
+                        die(header("Location: home.php"));
                     } else {
                         flash("Invalid password");
                     }
@@ -80,8 +79,8 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         } catch (Exception $e) {
             flash("<pre>" . var_export($e, true) . "</pre>");
         }
-
     }
 }
 ?>
-<?php require_once(__DIR__ . "/../partials/flash.php");
+<?php 
+require(__DIR__."/../partials/flash.php");
--- partials/nav.php
+++ partials/nav.php
@@ -1,21 +1,19 @@
 <?php
-require(__DIR__ . "/../lib/functions.php");
-
-//session_start(); must be the very first line after the <?php otherwise $_SESSION does not persist 
+/*
+require_once(__DIR__ . "/../lib/functions.php");
 //Note: this is to resolve cookie issues with port numbers
-
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
 $localWorks = true; //some people have issues with localhost for the cookie params
 //if you're one of those people make this false
-
+*/
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+if ($_SERVER["HTTP_HOST"] == "web.njit.edu") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
-        "path" => "/Project",
+        "path" => "$BASE_PATH",
         //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
         "domain" => $domain,
         "secure" => true,
@@ -23,35 +21,28 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
         "samesite" => "lax"
     ]);
 }
-
 session_start();
-//include functions here so we can have it on every page that uses the nav bar
-//that way we don't need to include so many other files on each page
-//nav will pull in functions and functions will pull in db
+
+require_once(__DIR__ . "/../lib/functions.php");
+
 ?>
+<!-- include css and js files -->
+<link rel="stylesheet" href="styles.css">
+<script src="helpers.js"></script>
+<link rel="stylesheet" href="styles.css">
+
 <nav>
     <ul>
-        <?php if (is_logged_in()) : 
-        ?> 
+        <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
-        <?php 
-            endif; 
-        ?>
-        <?php 
-//TODO 2        if (!is_logged_in()) : 
-        ?>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
-        <?php 
-//TODO 2        endif; 
-        ?>
-        <?php 
-//TODO 2        if (is_logged_in()) : 
-        ?>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
             <li><a href="logout.php">Logout</a></li>
-        <?php 
-//TODO 2        endif; 
-        ?>
-    </ul> 
+        <?php endif; ?>
+    </ul>
 </nav>
