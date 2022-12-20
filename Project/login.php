95e347b24515bc28f30ebe97e39a4105f5f4e15e
Yessica Quezada <ysq2@njit.edu>
2022-12-20 00:16:38 -0500

    milestone1

--- Project/login.php
+++ Project/login.php
@@ -1,144 +1,86 @@
 <?php
-require_once(__DIR__ . "/../partials/nav.php");
-is_logged_in(true);
+require(__DIR__ . "/../partials/nav.php");
 ?>
+<form onsubmit="return validate(this)" method="POST">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" required />
+    </div>
+    <div>
+        <label for="pw">Password</label>
+        <input type="password" id="pw" name="password" required minlength="8" />
+    </div>
+    <input type="submit" value="Login" />
+</form>
+<script>
+    function validate(form) {
+        //TODO 1: implement JavaScript validation
+        //ensure it returns false for an error and true for success
+
+        return true;
+    }
+</script>
 <?php
-if (isset($_POST["save"])) {
-    $email = se($_POST, "email", null, false);
-    $username = se($_POST, "username", null, false);
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
+
+    //TODO 3
     $hasError = false;
-    //sanitize
-    $email = sanitize_email($email);
-    //validate
-    if (!is_valid_email($email)) {
-        flash("Invalid email address", "danger");
+    if (empty($email)) {
+        flash("Email must not be empty");
         $hasError = true;
     }
-    if (!is_valid_username($username)) {
-        flash("Login Name must only contain 3-16 characters a-z, 0-9, _, or -", "danger");
+    //sanitize
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
+    //validate
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        flash("Invalid email address");
+        $hasError = true;
+    }*/
+    if (!is_valid_email($email)) {
+        flash("Invalid email address");
+        $hasError = true;
+    }
+    if (empty($password)) {
+        flash("password must not be empty");
+        $hasError = true;
+    }
+    if (!is_valid_password($password)) {
+        flash("Password too short");
         $hasError = true;
     }
     if (!$hasError) {
-        $params = [":email" => $email, ":username" => $username, ":id" => get_user_id()]; 
+        //flash("Welcome, $email");
+        //TODO 4
         $db = getDB();
-<<<<<<< HEAD
-        $stmt = $db->prepare("UPDATE User set email = :email, logName = :username where id = :id");
-=======
-        $stmt = $db->prepare("SELECT id, email, logName from User where id = :id LIMIT 1");
->>>>>>> 0a53478cbff66924d129f93bfaffc9096f5d4079
-        try {
-            $stmt->execute($params);
-            flash("Profile saved", "success");
-        } catch (Exception $e) {
-            users_check_duplicate($e->errorInfo);
-        }
-        //select fresh data from table
         $stmt = $db->prepare("SELECT id, email, logName from User where id = :id LIMIT 1");
         try {
-            $stmt->execute([":id" => get_user_id()]);
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($user) {
-                //$_SESSION["user"] = $user;
-                $_SESSION["user"]["email"] = $user["email"];
-                $_SESSION["user"]["logName"] = $user["logName"];
-            } else {
-                flash("User doesn't exist", "danger");
-            }
-        } catch (Exception $e) {
-            flash("An unexpected error occurred, please try again", "danger");
-            //echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
-        }
-    }
-
-
-    //check/update password
-    $current_password = se($_POST, "currentPassword", null, false);
-    $new_password = se($_POST, "newPassword", null, false);
-    $confirm_password = se($_POST, "confirmPassword", null, false);
-    if (!empty($current_password) && !empty($new_password) && !empty($confirm_password)) {
-        $hasError = false;
-        if (!is_valid_password($new_password)) {
-            flash("Password too short", "danger");
-            $hasError = true;
-        }
-        if (!$hasError) {
-            if ($new_password === $confirm_password) {
-                //TODO validate current
-                $stmt = $db->prepare("SELECT pwrdHash from User where id = :id");
-                try {
-                    $stmt->execute([":id" => get_user_id()]);
-                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                    if (isset($result["pwrdHash"])) {
-                        if (password_verify($current_password, $result["pwrdHash"])) {
-                            $query = "UPDATE User set pwrdHash = :pwrdHash where id = :id";
-                            $stmt = $db->prepare($query);
-                            $stmt->execute([
-                                ":id" => get_user_id(),
-                                ":pwrdHash" => password_hash($new_password, PASSWORD_BCRYPT)
-                            ]);
-
-                            flash("Password reset", "success");
-                        } else {
-                            flash("Current password is invalid", "warning");
-                        }
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["pwrdHash"];
+                    unset($user["pwrdHash"]);
+                    if (password_verify($password, $hash)) {
+                        //flash("Weclome $email");
+                        $_SESSION["user"] = $user; //sets our session data from db
+                        flash("Welcome, " . get_username());
+                        die(header("Location: home.php"));
+                    } else {
+                        flash("Invalid password");
                     }
-                } catch (Exception $e) {
-                    echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
+                } else {
+                    flash("Email not found");
                 }
-            } else {
-                flash("New passwords don't match", "warning");
             }
+        } catch (Exception $e) {
+            flash("<pre>" . var_export($e, true) . "</pre>");
         }
     }
 }
 ?>
-
-<?php
-$email = get_user_email();
-$username = get_username();
-?>
-<form method="POST" onsubmit="return validate(this);">
-    <div class="mb-3">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" value="<?php se($email); ?>" />
-    </div>
-    <div class="mb-3">
-        <label for="username">Login Name</label>
-        <input type="text" name="username" id="username" value="<?php se($username); ?>" />
-    </div>
-    <!-- DO NOT PRELOAD PASSWORD -->
-    <div>Password Reset</div>
-    <div class="mb-3">
-        <label for="cp">Current Password</label>
-        <input type="password" name="currentPassword" id="cp" autocomplete="off" />
-    </div>
-    <div class="mb-3">
-        <label for="np">New Password</label>
-        <input type="password" name="newPassword" id="np" />
-    </div>
-    <div class="mb-3">
-        <label for="conp">Confirm Password</label>
-        <input type="password" name="confirmPassword" id="conp" />
-    </div>
-    <input type="submit" value="Update Profile" name="save" />
-</form>
-
-<script>
-    function validate(form) {
-        let pw = form.newPassword.value;
-        let con = form.confirmPassword.value;
-        let isValid = true;
-        //TODO add other client side validation....
-
-        //example of using flash via javascript
-        //find the flash container, create a new element, appendChild
-        if (pw !== con) {
-            flash("Password and Confrim password must match", "warning");
-            isValid = false;
-        }
-        return isValid;
-    }
-</script>
-<?php
-require_once(__DIR__ . "/../partials/flash.php");
-?>
+<?php 
+require(__DIR__."/../partials/flash.php");
